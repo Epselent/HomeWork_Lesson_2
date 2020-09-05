9c6aabec9c3175e5e9466fdeba6de3ca607147d4
epselent <1597538426Den>
2020-09-05 09:23:19 +0500

    Шестое задание Upd

--- src/Task6.java
+++ src/Task6.java
@@ -5,20 +5,14 @@ public class Task6 {
         System.out.println("Введите целое число");
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        if (num > 0){
-            if (num%2 == 0) {
-                System.out.println("Число положительное четное");
-            }else {
-                System.out.println("Число положительное нечетное");
-            }
-        }else if (num !=0){
-            if (num%2 == 0) {
-                System.out.println("Число отрицательное четное");
-            }else {
-                System.out.println("Число отрицательное нечетное");
-            }
-        }else {
+        if (num == 0){
             System.out.println("Число нулевое");
-        }
+        }else if (num%2 == 0){
+            if (num > 0){
+                System.out.println("Число положительное четное");
+            }else  System.out.println("Число отрицательное четное");
+        }else if (num < 0){
+            System.out.println("Число отрицательное нечетное");
+        }else System.out.println("Число положительное нечетное");
     }
 }
